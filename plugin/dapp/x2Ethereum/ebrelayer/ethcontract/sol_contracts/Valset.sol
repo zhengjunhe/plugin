04a658430e5e8866a9a44dd9e88d886cf60bf164
hzj <hzj@disanbo.com>
2020-04-24 09:56:07 +0800

    simplefy solidity contract depend

--- plugin/dapp/x2Ethereum/ebrelayer/ethcontract/sol_contracts/Valset.sol
+++ plugin/dapp/x2Ethereum/ebrelayer/ethcontract/sol_contracts/Valset.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
-import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "../node_modules/openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
+import "../openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "../openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
 
 contract Valset {
 
