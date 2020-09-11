5815158877134123fad48f6929f828dd218cfa16
hzj <hzj@disanbo.com>
2020-09-11 15:40:25 +0800

    make jvm in go bind to env in c

---
+++ plugin/dapp/jvm/executor/adapter.go
@@ -0,0 +1,117 @@
+package executor
+
+//#cgo CFLAGS: -DTARGET_ARCH_x86 -DLINUX -DTARGET_COMPILER_gcc -DFULL_VERSION=1.8.0_262-internal-hezhengjun_2020_08_04_23_54-b00 -DJDK_MAJOR_VERSION=1 -DJDK_MINOR_VERSION=8
+//#cgo CFLAGS: -I/home/hezhengjun/work/c_code/openjdk/jvm4chain33/hotspot/src/share/vm
+//#cgo CFLAGS: -I/home/hezhengjun/work/c_code/openjdk/jvm4chain33/hotspot/src/share/vm/prims
+//#cgo CFLAGS: -I/home/hezhengjun/work/c_code/openjdk/jvm4chain33/hotspot/src/cpu/x86/vm
+//#cgo CFLAGS: -I/home/hezhengjun/work/c_code/openjdk/jvm4chain33/jdk/src/share/javavm/export
+//#cgo CFLAGS: -I/home/hezhengjun/work/c_code/openjdk/jvm4chain33/jdk/src/solaris/javavm/export
+//#cgo CFLAGS: -I/home/hezhengjun/work/c_code/openjdk/jvm4chain33/jdk/src/share/native/common
+//#cgo CFLAGS: -I/home/hezhengjun/work/c_code/openjdk/jvm4chain33/jdk/src/solaris/native/common
+//#cgo CFLAGS: -I/home/hezhengjun/work/c_code/openjdk/jvm4chain33/jdk/src/share/bin
+//#cgo CFLAGS: -I/home/hezhengjun/work/c_code/openjdk/jvm4chain33/jdk/src/solaris/bin
+//#cgo CFLAGS: -I/home/hezhengjun/work/c_code/openjdk/jvm4chain33/jdk/src/linux/bin
+//#cgo LDFLAGS: -L/home/hezhengjun/work/go/src/github.com/33cn/plugin/plugin/dapp/jvm/so -ljli
+//#cgo LDFLAGS: -ldl -lpthread -lc
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <stdint.h>
+//#include <defines.h>
+//int GetPtrSize() {
+//	return sizeof(char *);
+//}
+//void SetPtr(char **ptr, char *value, int index) {
+//	ptr[index] = value;
+//}
+//void FreeArgv(int argc, char **argv) {
+//    int i = 0;
+//    for (; i < argc; i++) {
+//        free(argv[i]);
+//    }
+//    free(argv);
+//}
+//char ** GetNil2dPtr() {
+//	return (char **)0;
+//}
+//void * GetVoidPtr(char *voidPtr) {
+//	return (void *)voidPtr;
+//}
+import "C"
+
+
+import (
+	"errors"
+	"unsafe"
+)
+
+const (
+	JLI_SUCCESS = int(0)
+	JLI_FAIL    = int(-1)
+	TX_EXEC_JOB = C.int(0)
+	TX_QUERY_JOB = C.int(1)
+)
+
+var (
+	jvm_init_alreay = false
+)
+
+//调用java合约交易
+func runJava(contract, action string, para []string, cp string, jvmHandleGo *JVMExecutor) error {
+	//第一次调用java合约时，进行jvm的初始化
+	initJvm(cp)
+
+	//执行合约
+	// -jar contract action para0 para1 ...
+	//tx2Exec := "PrintArgs debug chain33-jvm I am an Engineer !"
+	tx2Exec := append([]string{"-jar", contract, action}, para...)
+	argc, argv := buildJavaArgument(tx2Exec)
+	defer freeArgument(argc, argv)
+	var exception1DPtr *C.char
+	exception := &exception1DPtr
+
+	value := unsafe.Pointer(uintptr(unsafe.Pointer(jvmHandleGo)))
+	ptr := C.GetVoidPtr((*C.char)(value))
+
+	result := C.JLI_Exec_Contract(argc, argv, exception, TX_EXEC_JOB, ptr)
+	if int(result) != JLI_SUCCESS {
+		exInfo := C.GoString(*exception)
+		defer C.free(unsafe.Pointer(*exception))
+		log.Debug("adapter::runJava", "java exception", exInfo)
+		return errors.New(exInfo)
+	}
+	return nil
+}
+
+func initJvm(cp string) {
+	if jvm_init_alreay {
+		return
+	}
+	const_cp := C.CString(cp)
+	defer C.free(unsafe.Pointer(const_cp))
+	result := C.JLI_Create_TxJVM(const_cp)
+	if int(result) != JLI_SUCCESS {
+		panic("Failed to init JLI_Init_JVM")
+	}
+	jvm_init_alreay = true
+}
+
+func buildJavaArgument(execPara []string) (C.int, **C.char) {
+	argc := C.int(len(execPara))
+
+	nil2dPtr := C.GetNil2dPtr()
+	argv := (**C.char)(C.malloc(C.ulong(argc * C.GetPtrSize())))
+	if argv == nil2dPtr {
+		panic("Failed to malloc for argv")
+	}
+	//argv [argc]*C.char
+	for i, para := range execPara {
+		paraCstr := C.CString(para)
+		C.SetPtr(argv, paraCstr, C.int(i))
+	}
+	return argc, argv
+}
+
+func freeArgument(argc C.int, argv **C.char) {
+	C.FreeArgv(argc, argv)
+}
--- plugin/dapp/jvm/so/cb4java.go
+++ plugin/dapp/jvm/so/cb4java.go
@@ -5,52 +5,60 @@ package main
 import "C"
 
 import (
-	//"github.com/33cn/plugin/plugin/dapp/jvm/executor"
-	"github.com/33cn/plugin/plugin/dapp/jvm/so/test/executor"
+	"github.com/33cn/plugin/plugin/dapp/jvm/executor"
+	//"github.com/33cn/plugin/plugin/dapp/jvm/so/test/executor"
 	"unsafe"
 )
-
-//var Nil_info_ptr *C.char
 
 const (
 	Bool_TRUE  = C.int(1)
 	Bool_FALSE = C.int(0)
-	//NIL_INFO   = "magic-data-nil-0000"
 )
 
-//func init() {
-//	Nil_info_ptr = C.CString(NIL_INFO)
-//}
+//用来保存txjvm或者是queryjvm中的env handle
+//export BindTxQueryJVMEnvHandle
+func BindTxQueryJVMEnvHandle(jvmGoHandle, envHandle *C.char) C.int {
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	jvmExecutor := (*executor.JVMExecutor)(unsafe.Pointer(jvmGoHandle))
+	if !executor.RecordTxJVMEnv(jvmExecutor, envHandleUintptr) {
+		return Bool_FALSE
+	}
+	return Bool_TRUE
+}
+
 /*
  * Account
  */
 //export ExecFrozen
-func ExecFrozen(from *C.char, amount C.long) C.int {
+func ExecFrozen(from *C.char, amount C.long, envHandle *C.char) C.int {
 	defer C.free(unsafe.Pointer(from))
 	fromGoStr := C.GoString(from)
-	if !executor.ExecFrozen(fromGoStr, int64(amount)) {
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	if !executor.ExecFrozen(fromGoStr, int64(amount), envHandleUintptr) {
 		return Bool_FALSE
 	}
 	return Bool_TRUE
 }
 
 //export ExecActive
-func ExecActive(from *C.char, amount C.long) C.int {
+func ExecActive(from *C.char, amount C.long, envHandle *C.char) C.int {
 	defer C.free(unsafe.Pointer(from))
 	fromGoStr := C.GoString(from)
-	if !executor.ExecActive(fromGoStr, int64(amount)) {
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	if !executor.ExecActive(fromGoStr, int64(amount), envHandleUintptr) {
 		return Bool_FALSE
 	}
 	return Bool_TRUE
 }
 
 //export ExecTransfer
-func ExecTransfer(from, to *C.char, amount C.long) C.int {
+func ExecTransfer(from, to *C.char, amount C.long, envHandle *C.char) C.int {
 	defer C.free(unsafe.Pointer(from))
 	defer C.free(unsafe.Pointer(to))
 	fromGoStr := C.GoString(from)
 	toGoStr := C.GoString(to)
-	if !executor.ExecTransfer(fromGoStr, toGoStr, int64(amount)) {
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	if !executor.ExecTransfer(fromGoStr, toGoStr, int64(amount), envHandleUintptr) {
 		return Bool_FALSE
 	}
 	return Bool_TRUE
@@ -61,8 +69,9 @@ func ExecTransfer(from, to *C.char, amount C.long) C.int {
  */
 //调用者负责释放返回指针内存
 //export GetRandom
-func GetRandom() *C.char {
-	random, err := executor.GetRandom()
+func GetRandom(envHandle *C.char) *C.char {
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	random, err := executor.GetRandom(envHandleUintptr)
 	if nil != err {
 		return nil
 	}
@@ -71,21 +80,24 @@ func GetRandom() *C.char {
 
 //调用者负责释放返回指针内存
 //export GetFrom
-func GetFrom() *C.char {
-	from := executor.GetFrom()
+func GetFrom(envHandle *C.char) *C.char {
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	from := executor.GetFrom(envHandleUintptr)
 	return C.CString(from)
 }
 
 //export GetCurrentHeight
-func GetCurrentHeight() C.long {
-	return C.long(executor.GetHeight())
+func GetCurrentHeight(envHandle *C.char) C.long {
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	return C.long(executor.GetHeight(envHandleUintptr))
 }
 
 //export StopTransWithErrInfo
-func StopTransWithErrInfo(errInfo *C.char) C.int {
+func StopTransWithErrInfo(errInfo *C.char, envHandle *C.char) C.int {
 	defer C.free(unsafe.Pointer(errInfo))
 	errInfoStr := C.GoString(errInfo)
-	executor.StopTransWithErrInfo(errInfoStr)
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	executor.StopTransWithErrInfo(errInfoStr, envHandleUintptr)
 
 	return Bool_TRUE
 }
@@ -94,10 +106,11 @@ func StopTransWithErrInfo(errInfo *C.char) C.int {
  * State DB
  */
 //export SetState
-func SetState(key *C.char, keySize C.int, value *C.char, valueSize C.int) C.int {
+func SetState(key *C.char, keySize C.int, value *C.char, valueSize C.int, envHandle *C.char) C.int {
 	keySlice := C.GoBytes(unsafe.Pointer(key), keySize)
 	valSlice := C.GoBytes(unsafe.Pointer(value), valueSize)
-	if !executor.StateDBSetState(keySlice, valSlice) {
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	if !executor.StateDBSetState(keySlice, valSlice, envHandleUintptr) {
 		return Bool_FALSE
 	}
 	return Bool_TRUE
@@ -105,21 +118,22 @@ func SetState(key *C.char, keySize C.int, value *C.char, valueSize C.int) C.int 
 
 //需要调用者释放内存
 //export GetFromState
-func GetFromState(key *C.char, keySize C.int, valueSize *C.int) *C.char {
+func GetFromState(key *C.char, keySize C.int, valueSize *C.int, envHandle *C.char) *C.char {
 	keySlice := C.GoBytes(unsafe.Pointer(key), keySize)
-	value := executor.StateDBGetState(keySlice)
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	value := executor.StateDBGetState(keySlice, envHandleUintptr)
 	*valueSize = C.int(len(value))
 	return (*C.char)(C.CBytes(value))
 }
 
 //export SetStateInStr
-func SetStateInStr(key *C.char, value *C.char) C.int {
+func SetStateInStr(key *C.char, value *C.char, envHandle *C.char) C.int {
 	defer C.free(unsafe.Pointer(key))
 	defer C.free(unsafe.Pointer(value))
 	keyStr := C.GoString(key)
 	valueStr := C.GoString(value)
-
-	if !executor.StateDBSetState([]byte(keyStr), []byte(valueStr)) {
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	if !executor.StateDBSetState([]byte(keyStr), []byte(valueStr), envHandleUintptr) {
 		return Bool_FALSE
 	}
 	return Bool_TRUE
@@ -127,14 +141,15 @@ func SetStateInStr(key *C.char, value *C.char) C.int {
 
 //调用者负责释放返回指针内存
 //export GetFromStateInStr
-func GetFromStateInStr(key *C.char, size *C.int) *C.char {
+func GetFromStateInStr(key *C.char, size *C.int, envHandle *C.char) *C.char {
 	defer C.free(unsafe.Pointer(key))
 	keyStr := C.GoString(key)
 	if "" == keyStr {
 		*size = C.int(0)
 		return nil
 	}
-	valueSlice := executor.StateDBGetState([]byte(keyStr))
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	valueSlice := executor.StateDBGetState([]byte(keyStr), envHandleUintptr)
 	valSize := len(valueSlice)
 	if 0 == valSize {
 		*size = C.int(0)
@@ -148,31 +163,33 @@ func GetFromStateInStr(key *C.char, size *C.int) *C.char {
  * Local DB
  */
 //export SetLocal
-func SetLocal(key *C.char, keySize C.int, value *C.char, valueSize C.int) C.int {
+func SetLocal(key *C.char, keySize C.int, value *C.char, valueSize C.int, envHandle *C.char) C.int {
 	keySlice := C.GoBytes(unsafe.Pointer(key), keySize)
 	valSlice := C.GoBytes(unsafe.Pointer(value), valueSize)
-	if !executor.SetValue2Local(keySlice, valSlice) {
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	if !executor.SetValue2Local(keySlice, valSlice, envHandleUintptr) {
 		return Bool_FALSE
 	}
 	return Bool_TRUE
 }
 
 //export GetFromLocal
-func GetFromLocal(key *C.char, keySize C.int, valueSize *C.int) *C.char {
+func GetFromLocal(key *C.char, keySize C.int, valueSize *C.int, envHandle *C.char) *C.char {
 	keySlice := C.GoBytes(unsafe.Pointer(key), keySize)
-	value := executor.GetValueFromLocal(keySlice)
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	value := executor.GetValueFromLocal(keySlice, envHandleUintptr)
 	*valueSize = C.int(len(value))
 	return (*C.char)(C.CBytes(value))
 }
 
 //export SetLocalInStr
-func SetLocalInStr(key *C.char, value *C.char) C.int {
+func SetLocalInStr(key *C.char, value *C.char, envHandle *C.char) C.int {
 	defer C.free(unsafe.Pointer(key))
 	defer C.free(unsafe.Pointer(value))
 	keyStr := C.GoString(key)
 	valueStr := C.GoString(value)
-
-	if !executor.SetValue2Local([]byte(keyStr), []byte(valueStr)) {
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	if !executor.SetValue2Local([]byte(keyStr), []byte(valueStr), envHandleUintptr) {
 		return Bool_FALSE
 	}
 	return Bool_TRUE
@@ -180,14 +197,15 @@ func SetLocalInStr(key *C.char, value *C.char) C.int {
 
 //调用者负责释放返回指针内存
 //export GetFromLocalInStr
-func GetFromLocalInStr(key *C.char, size *C.int) *C.char {
+func GetFromLocalInStr(key *C.char, size *C.int, envHandle *C.char) *C.char {
 	defer C.free(unsafe.Pointer(key))
 	keyStr := C.GoString(key)
 	if "" == keyStr {
 		*size = C.int(0)
 		return nil
 	}
-	valueSlice := executor.GetValueFromLocal([]byte(keyStr))
+	envHandleUintptr := uintptr(unsafe.Pointer(envHandle))
+	valueSlice := executor.GetValueFromLocal([]byte(keyStr), envHandleUintptr)
 	valSize := len(valueSlice)
 	if 0 == valSize {
 		*size = C.int(0)
