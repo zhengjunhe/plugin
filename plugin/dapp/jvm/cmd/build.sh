4082d4466e03c036edad4151ed96a21757b89dca
QM <yingqiumin@126.com>
2020-11-12 16:08:04 +0800

    add rpc test“ ”

--- plugin/dapp/jvm/cmd/build.sh
+++ plugin/dapp/jvm/cmd/build.sh
@@ -4,6 +4,12 @@ strpwd=$(pwd)
 strcmd=${strpwd##*dapp/}
 strapp=${strcmd%/cmd*}
 
+OUT_DIR="${1}/$strapp"
+mkdir -p "${OUT_DIR}"
+# shellcheck disable=SC2086
+cp -r ./build/* "${OUT_DIR}"
+
 OUT_TESTDIR="${1}/dapptest/$strapp"
 mkdir -p "${OUT_TESTDIR}"
-cp ./test/test-rpc.sh "${OUT_TESTDIR}"
+cp ./test/* "${OUT_TESTDIR}"
+
