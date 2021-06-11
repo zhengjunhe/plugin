f1697acb1119596717be11e8268ad490aabd214f
QM <yingqiumin@126.com>
2021-06-11 13:57:44 +0800

    update cross2eth test

--- plugin/dapp/cross2eth/cmd/build/relayerPublic.sh
+++ plugin/dapp/cross2eth/cmd/build/relayerPublic.sh
@@ -28,13 +28,11 @@ chain33ReceiverAddrKey="4257d8692ef7fe13c68b65d6a52f03933db2fa5ce8faf210b5b8b80c
 
 # 新增地址 chain33 需要导入地址 转入 10 bty当收费费
 # shellcheck disable=SC2034
-#chain33ValidatorA="1GTxrmuWiXavhcvsaH5w9whgVxUrWsUMdV"
 chain33ValidatorA="14KEKbYtKKQm4wMthSK9J4La4nAiidGozt"
 chain33ValidatorB="155ooMPBTF8QQsGAknkK7ei5D78rwDEFe6"
 chain33ValidatorC="13zBdQwuyDh7cKN79oT2odkxYuDbgQiXFv"
 chain33ValidatorD="113ZzVamKfAtGt9dq45fX1mNsEoDiN95HG"
-#chain33ValidatorKeyA="0xd627968e445f2a41c92173225791bae1ba42126ae96c32f28f97ff8f226e5c68"
-chain33ValidatorKeyA="0xcc38546e9e659d15e6b4893f0ab32a06d103931a8230b0bde71459d2b27d6944"
+#chain33ValidatorKeyA="0xcc38546e9e659d15e6b4893f0ab32a06d103931a8230b0bde71459d2b27d6944"
 # shellcheck disable=SC2034
 {
 chain33ValidatorKeyB="0x9d539bc5fd084eb7fe86ad631dba9aa086dba38418725c38d9751459f567da66"
@@ -49,6 +47,7 @@ BridgeRegistryOnChain33=""
 chain33BridgeBank=""
 BridgeRegistryOnEth=""
 ethBridgeBank=""
+chain33BtyTokenAddr="1111111111111111111114oLvT2"
 
 #
 chain33EthTokenAddr=""
@@ -343,7 +342,7 @@ function StartChain33() {
     # delete chain33 datadir
     rm ../../datadir ../../logs -rf
 
-    nohup ../../chain33 -f ./ci/cross2eth/test.toml 2>&1 &
+    nohup ../../chain33 -f ./ci/cross2eth/test.toml > chain33log.log 2>&1 &
 
     sleep 1
 
@@ -396,4 +395,150 @@ function StartRelayerOnRopsten() {
     echo -e "${GRE}=========== $FUNCNAME begin ===========${NOC}"
 
     echo -e "${GRE}=========== $FUNCNAME end ===========${NOC}"
+}
+
+# shellcheck disable=SC2034
+{
+chain33MultisignA="168Sn1DXnLrZHTcAM9stD6t2P49fNuJfJ9"
+chain33MultisignB="13KTf57aCkVVJYNJBXBBveiA5V811SrLcT"
+chain33MultisignC="1JQwQWsShTHC4zxHzbUfYQK4kRBriUQdEe"
+chain33MultisignD="1NHuKqoKe3hyv52PF8XBAyaTmJWAqA2Jbb"
+chain33MultisignKeyA="0xcd284cd17456b73619fa609bb9e3105e8eff5d059c5e0b6eb1effbebd4d64144"
+chain33MultisignKeyB="0xe892212221b3b58211b90194365f4662764b6d5474ef2961ef77c909e31eeed3"
+chain33MultisignKeyC="0x9d19a2e9a440187010634f4f08ce36e2bc7b521581436a99f05568be94dc66ea"
+chain33MultisignKeyD="0x45d4ce009e25e6d5e00d8d3a50565944b2e3604aa473680a656b242d9acbff35"
+
+ethMultisignA=0x4c85848a7E2985B76f06a7Ed338FCB3aF94a7DCf
+ethMultisignB=0x6F163E6daf0090D897AD7016484f10e0cE844994
+ethMultisignC=0xbc333839E37bc7fAAD0137aBaE2275030555101f
+ethMultisignD=0x495953A743ef169EC5D4aC7b5F786BF2Bd56aFd5
+ethMultisignKeyA=0x5e8aadb91eaa0fce4df0bcc8bd1af9e703a1d6db78e7a4ebffd6cf045e053574
+ethMultisignKeyB=0x0504bcb22b21874b85b15f1bfae19ad62fc2ad89caefc5344dc669c57efa60db
+ethMultisignKeyC=0x0c61f5a879d70807686e43eccc1f52987a15230ae0472902834af4d1933674f2
+ethMultisignKeyD=0x2809477ede1261da21270096776ba7dc68b89c9df5f029965eaa5fe7f0b80697
+}
+
+function deployMultisign() {
+    echo -e "${GRE}=========== $FUNCNAME begin ===========${NOC}"
+    for name in A B C D; do
+        eval chain33MultisignKey=\$chain33MultisignKey${name}
+        eval chain33Multisign=\$chain33Multisign${name}
+        # shellcheck disable=SC2154
+        result=$(${Chain33Cli} account import_key -k "${chain33MultisignKey}" -l multisignAddr$name)
+        # shellcheck disable=SC2154
+        check_addr "${result}" "${chain33Multisign}"
+
+        # chain33Multisign 要有手续费
+        hash=$(${Chain33Cli} send coins transfer -a 10 -t "${chain33Multisign}" -k "${chain33DeployAddr}")
+        check_tx "${Chain33Cli}" "${hash}"
+        result=$(${Chain33Cli} account balance -a "${chain33Multisign}" -e coins)
+        balance_ret "${result}" "10.0000"
+    done
+
+    echo -e "${GRE}=========== 部署 chain33 离线钱包合约 ===========${NOC}"
+    result=$(${CLIA} chain33 multisign deploy)
+    cli_ret "${result}" "chain33 multisign deploy"
+    multisignChain33Addr=$(echo "${result}" | jq -r ".msg")
+
+    result=$(${CLIA} chain33 multisign setup -k "${chain33DeployKey}" -o "${chain33MultisignA},${chain33MultisignB},${chain33MultisignC},${chain33MultisignD}")
+    cli_ret "${result}" "chain33 multisign setup"
+
+    # multisignChain33Addr 要有手续费
+    hash=$(${Chain33Cli} send coins transfer -a 10 -t "${multisignChain33Addr}" -k "${chain33DeployAddr}")
+    check_tx "${Chain33Cli}" "${hash}"
+    result=$(${Chain33Cli} account balance -a "${multisignChain33Addr}" -e coins)
+    balance_ret "${result}" "10.0000"
+
+    hash=$(${Chain33Cli} evm call -f 1 -c "${chain33DeployAddr}" -e "${chain33BridgeBank}" -p "configOfflineSaveAccount(${multisignChain33Addr})")
+    check_tx "${Chain33Cli}" "${hash}"
+
+    echo -e "${GRE}=========== 部署 ETH 离线钱包合约 ===========${NOC}"
+    result=$(${CLIA} ethereum multisign deploy)
+    cli_ret "${result}" "ethereum multisign deploy"
+    multisignEthAddr=$(echo "${result}" | jq -r ".msg")
+
+    result=$(${CLIA} ethereum multisign setup -k "${ethDeployKey}" -o "${ethMultisignA},${ethMultisignB},${ethMultisignC},${ethMultisignD}")
+    cli_ret "${result}" "ethereum multisign setup"
+
+    result=$(${CLIA} ethereum multisign set_offline_addr -s "${multisignEthAddr}")
+    cli_ret "${result}" "set_offline_addr"
+
+    echo -e "${GRE}=========== $FUNCNAME end ===========${NOC}"
+}
+
+# lock bty 判断是否转入多签地址金额是否正确
+function lock_bty_multisign () {
+    local lockAmount=$1
+    local lockAmount2="${1}00000000"
+    hash=$(${Chain33Cli} evm call -f 1 -a "${lockAmount}" -c "${chain33DeployAddr}" -e "${chain33BridgeBank}" -p "lock(${ethDeployAddr}, ${chain33BtyTokenAddr}, ${lockAmount2})")
+    check_tx "${Chain33Cli}" "${hash}"
+
+    if [[ $# -eq 3 ]]; then
+        local bridgeBankBalance=$2
+        local multisignBalance=$3
+
+        result=$(${Chain33Cli} account balance -a "${chain33BridgeBank}" -e evm)
+        balance_ret "${result}" "${bridgeBankBalance}"
+        result=$(${Chain33Cli} account balance -a "${multisignChain33Addr}" -e evm)
+        balance_ret "${result}" "${multisignBalance}"
+    fi
+}
+
+# lock chain33 ycc erc20 判断是否转入多签地址金额是否正确
+function lock_chain33_ycc_multisign () {
+    local lockAmount="${1}00000000"
+    hash=$(${Chain33Cli} evm call -f 1 -c "${chain33DeployAddr}" -e "${chain33BridgeBank}" -p "lock(${ethDeployAddr}, ${chain33YccErc20Addr}, ${lockAmount})")
+    check_tx "${Chain33Cli}" "${hash}"
+
+    if [[ $# -eq 3 ]]; then
+        local bridgeBankBalance="${2}00000000"
+        local multisignBalance="${3}00000000"
+        if [[ "${3}" == "0" ]]; then
+            multisignBalance="0"
+        fi
+
+        result=$(${Chain33Cli} evm abi call -a "${chain33YccErc20Addr}" -c "${chain33BridgeBank}" -b "balanceOf(${chain33BridgeBank})")
+        is_equal "${result}" "${bridgeBankBalance}"
+        result=$(${Chain33Cli} evm abi call -a "${chain33YccErc20Addr}" -c "${multisignChain33Addr}" -b "balanceOf(${multisignChain33Addr})")
+        is_equal "${result}" "${multisignBalance}"
+    fi
+}
+
+# lock eth 判断是否转入多签地址金额是否正确
+function lock_eth_multisign() {
+    local lockAmount=$1
+    result=$(${CLIA} ethereum lock -m "${lockAmount}" -k "${ethDeployKey}" -r "${chain33ReceiverAddr}")
+    cli_ret "${result}" "lock"
+
+    if [[ $# -eq 3 ]]; then
+        local bridgeBankBalance=$2
+        local multisignBalance=$3
+        # eth 等待 10 个区块
+        eth_block_wait 2
+
+        result=$(${CLIA} ethereum balance -o "${ethBridgeBank}" )
+        cli_ret "${result}" "balance" ".balance" "${bridgeBankBalance}"
+        result=$(${CLIA} ethereum balance -o "${multisignEthAddr}" )
+        cli_ret "${result}" "balance" ".balance" "${multisignBalance}"
+    fi
+}
+
+# lock ethereum ycc erc20 判断是否转入多签地址金额是否正确
+function lock_ethereum_ycc_multisign() {
+    local lockAmount=$1
+    result=$(${CLIA} ethereum lock -m "${lockAmount}" -k "${ethDeployKey}" -r "${chain33ReceiverAddr}" -t "${ethereumYccTokenAddr}")
+    cli_ret "${result}" "lock"
+
+    if [[ $# -eq 3 ]]; then
+        local bridgeBankBalance=$2
+        local multisignBalance=$3
+
+        # eth 等待 10 个区块
+        eth_block_wait 2
+
+        result=$(${CLIA} ethereum balance -o "${ethBridgeBank}" -t "${ethereumYccTokenAddr}")
+        cli_ret "${result}" "balance" ".balance" "${bridgeBankBalance}"
+        result=$(${CLIA} ethereum balance -o "${multisignEthAddr}" -t "${ethereumYccTokenAddr}")
+        cli_ret "${result}" "balance" ".balance" "${multisignBalance}"
+    fi
 }
